06d79afa18593ff03ccf85d44d3b31d484ec5dfb
Sam Joyce <samjoyce777@gmail.com>
2014-06-25 12:21:20 +0100

    added auth restriction to admin

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -2,7 +2,11 @@
 namespace Larapress\Larapress;
 
 class AdminController extends \BaseController{
-    
+
+    public function __construct(){
+        $this->beforeFilter('auth');
+    }
+
     public function index(){
         
         return \View::make('larapress::index');
